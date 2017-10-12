e2a45f26183cea823f3f951d5d0a578c4629da09
xiaobinbin7530 <2523901638@qq.com>
2017-10-12 09:31:09 +0800

    翻墙

---
+++ 二维码/EncodingUtils.java
@@ -0,0 +1,80 @@
+package tv.fuzegame.fuzelauncher.ui.utils;
+
+import android.graphics.Bitmap;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import java.util.Hashtable;
+
+/**
+ * 二维码工具类
+ *
+ * @author LiuLei
+ */
+public class EncodingUtils {
+    private static final int BLACK = 0xff000000;
+
+    public static Bitmap createQRCode(String str, int widthAndHeight) throws WriterException {
+        Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+        BitMatrix matrix = new MultiFormatWriter().encode(str, BarcodeFormat.QR_CODE, widthAndHeight, widthAndHeight, hints);
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        int[] pixels = new int[width * height];
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (matrix.get(x, y)) {
+                    pixels[y * width + x] = BLACK;
+                } 
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
+
+
+
+
+    public static Bitmap createQRCodeChange(String str, int widthAndHeight) throws WriterException {
+        Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+        BitMatrix matrix = new MultiFormatWriter().encode(str,
+                BarcodeFormat.QR_CODE, widthAndHeight, widthAndHeight, hints);
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        int[] pixels = new int[width * height];
+        boolean isFirstBlackPoint = false;
+        int startX = 0;
+        int startY = 0;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (matrix.get(x, y)) {
+                    if (isFirstBlackPoint == false) {
+                        isFirstBlackPoint = true;
+                        startX = x;
+                        startY = y;
+                    }
+                    pixels[y * width + x] = BLACK;
+                }
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        if (startX <= 0) return bitmap;
+        int x1 = startX - 0;
+        int y1 = startY - 0;
+        if (x1 < 0 || y1 < 0) return bitmap;
+        int w1 = width - x1 * 2;
+        int h1 = height - y1 * 2;
+        Bitmap bitmapQR = Bitmap.createBitmap(bitmap, x1, y1, w1, h1);
+        return bitmapQR;
+    }
+
+
+}
